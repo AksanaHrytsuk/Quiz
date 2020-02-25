467344800bf1cc8bff4b574b86c692dcc5fb18e2
Oksana <kroshkaksju@gmail.com>
2020-02-25 04:51:05 +0300

    some kind of magic

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -6,75 +6,148 @@ using UnityEngine.SceneManagement;
 
 public class Quiz : MonoBehaviour
 {
-  int activeTask;
-  [Header("Elements")]
-  public Image heart1;
-  public Image heart2;
-  public Image heart3;
-  public Image question; //картинка-вопрос
-  public Button[] buttons; //кнопка-ответ
+    int activeTask;
+    int lives = 3;
 
-  public Button fiftyFifty;
+    [Header("Elements")]
+    public Image[] hearts;
+    public Image question; //картинка-вопрос
+    public Button[] buttons; //кнопка-ответ
 
-  [Header("Config")]
-  public List<Tasks> activeTasks;
-  
+    [Header("Config")]
+    public List<Tasks> taskList;
 
-
-  // Start is called before the first frame update
-  void Start()
-  {
-    DontDestroyOnLoad(gameObject);
-    getRandomTask();
-    fillButtons(activeTask);
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-
-  public void RemoveTask(int index){
-    activeTasks.RemoveAt(index);
-  }
-
-  void getRandomTask(){
-    activeTask = Random.Range(0,activeTasks.Count);
-  }
-
-  void fillButtons(int taskNumber)
-  {
-    for (int i = 0; i < 4; i++)
+    // Start is called before the first frame update
+    void Start()
     {
-      buttons[i].GetComponentInChildren<Text>().text = activeTasks[taskNumber].buttons[i];
+        // DontDestroyOnLoad(gameObject);
+        getRandomTask();
+        FillButtons();
+        UpdateQuestion();
     }
-  }
 
-
-
-  public void SwitchOffBtn(int[] index) //кнопка 50/50 вызывает данную функцию. 
-  {
-    foreach (int i in index)
+    // Update is called once per frame
+    void Update()
     {
-      buttons[i].gameObject.SetActive(false);
     }
-  }
-  public void SwitchOffHeart1() // выключает  первое сердце-жизнь
-  {
-    heart1.gameObject.SetActive(false);
-  }
-  public void SwitchOffHeart2()
-  {
-    heart1.gameObject.SetActive(false);
-  }
-  public void SwitchOffHeart3()
-  {
-    heart1.gameObject.SetActive(false);
-  }
-  public void NextScene(string sceneName) //загрузчик сцен
-  {
-    SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
-  }
+
+    public void RemoveTask()
+    {
+        taskList.RemoveAt(activeTask);
+    }
+
+    void getRandomTask()
+    {
+        activeTask = Random.Range(0, taskList.Count);
+    }
+
+    void LoadNextLevel()
+    {
+        RemoveTask();
+        getRandomTask();
+        FillButtons();
+        UpdateQuestion();
+        ActivateButtons();
+    }
+    void FillButtons()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            buttons[i].GetComponentInChildren<Text>().text = taskList[activeTask].buttons[i];
+        }
+    }
+    public void SwitchOffHearts()
+    {
+        hearts[lives - 1].gameObject.SetActive(false);
+        lives--;
+        //
+    }
+    void UpdateQuestion()
+    {
+        question.sprite = taskList[activeTask].question;
+    }
+
+    bool IsCorrect(int btnIndex)
+    {
+        if (btnIndex == taskList[activeTask].correctAnsver)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void GameOver()
+    {
+        if (lives == 0)
+        {
+            NextScene("GameOverScene");
+        }
+        else
+        {
+            LoadNextLevel();
+        }
+    }
+
+    void Win()
+    {
+        if (taskList.Count == 1)
+        {
+            NextScene("WinScene");
+        }
+        else
+        {
+            LoadNextLevel();
+        }
+    }
+
+    IEnumerator Wait(int btnIndex)
+    {
+        buttons[taskList[activeTask].correctAnsver - 1].GetComponent<Image>().color = Color.blue;
+        yield return new WaitForSeconds(1);
+        buttons[taskList[activeTask].correctAnsver - 1].GetComponent<Image>().color = Color.white;
+        CheckAnsver(btnIndex);
+    }
+    public void CheckAnsver(int btnIndex)
+    {
+
+        if (IsCorrect(btnIndex))
+        {
+            Win();
+        }
+        else
+        {
+            SwitchOffHearts();
+            GameOver();
+        }
+    }
+    public void OnClick(int btnIndex)
+    {
+        StartCoroutine(Wait(btnIndex));
+    }
+
+    void ActivateButtons()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            buttons[i].gameObject.SetActive(true);
+        }
+    }
+    public void FiftyFifty()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            buttons[taskList[activeTask].fiftyFifty[i] - 1].gameObject.SetActive(false);
+        }
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void NextScene(string sceneName) //загрузчик сцен
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
 }
--- Assets/Scripts/Tasks.cs
+++ Assets/Scripts/Tasks.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "Tasks", menuName = "Tasks", order = 0)]
 public class Tasks : ScriptableObject
 {
-  public Sprite quetion1;
+  public Sprite question;
   public string [] buttons = new string[4];
   public int correctAnsver;
 
